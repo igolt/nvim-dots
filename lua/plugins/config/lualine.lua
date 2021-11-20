1022708dc3a7aadb4464d2e9b3d367f3165a2ba1
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2021-11-19 21:33:37 -0300

    Change statusline config.

--- lua/plugins/config/lualine.lua
+++ lua/plugins/config/lualine.lua
@@ -2,6 +2,21 @@ require'lualine'.setup {
   options = {
     theme = 'auto',
     disabled_filetypes = {'dashboard'},
+    component_separators= '|',
+    section_separators = { left = '', right = '' }
+  },
+  sections = {
+    lualine_a = {
+      {'mode', separator = { left = '' }, padding = {left = 1, right = 2}}
+    },
+    lualine_b = {'branch', 'diff',
+    {'diagnostics', sources={'nvim_lsp'}}, {'filename', path = 1}},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {'filetype', 'fileformat', 'encoding'},
+    lualine_z = {
+      {'location', separator = {right = ''}}
+    }
   },
   extensions = {'fugitive', 'nvim-tree', 'toggleterm', 'quickfix'}
 }
