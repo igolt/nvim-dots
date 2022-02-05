3d1d1c4e2d6fc2aa84e78b76386eb187c4f6dbea
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2022-02-05 01:12:30 -0300

    C code runner.

--- after/ftplugin/c.vim
+++ after/ftplugin/c.vim
@@ -1,1 +1,3 @@
 setlocal tabstop=4
+
+nnoremap <leader>rc <cmd>lua require('term_extensions').c_code_runner()<cr>
--- lua/term_extensions/init.lua
+++ lua/term_extensions/init.lua
@@ -1,16 +1,16 @@
 local Terminal = require('toggleterm.terminal').Terminal
 
-return {
-  python_code_runner = function ()
-    local filepath = ('"%s"'):format(vim.api.nvim_buf_get_name(0))
+local code_runner = function (cmd)
+  local filepath = ('"%s"'):format(vim.api.nvim_buf_get_name(0))
     local term = Terminal:new({
-      cmd = 'clear; python ' .. filepath .. '; printf "\\nPress enter..."; read -r nothing; exit',
-      direction = 'float',
-      float_opts = {
-        border = 'curved',
-      },
+      cmd = 'clear; ' .. cmd .. ' ' .. filepath .. '; printf "\\nPress enter..."; read -r nothing; exit',
+      direction = 'horizontal',
       close_on_exit = true,
     })
     term:toggle()
-  end,
+end
+
+return {
+  python_code_runner = function () code_runner('python') end,
+  c_code_runner = function () code_runner('. ~/.config/bash/functions; csource') end,
 }
