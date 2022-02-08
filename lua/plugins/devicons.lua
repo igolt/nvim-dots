1f936ac54cf58d6361bf4b6ddd116a2d56bafd6a
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2022-02-08 00:14:01 -0300

    Adding an icon to 'Makefile'

--- lua/plugins/devicons.lua
+++ lua/plugins/devicons.lua
@@ -1,7 +1,16 @@
-require('nvim-web-devicons').set_icon({
-  js = {
-    icon = "",
-    name = "Js",
-    color = '#f7df1e'
-  }
-})
+require('nvim-web-devicons').setup {
+  override = {
+    js = {
+      icon = "",
+      name = "Js",
+      color = '#f7df1e'
+    },
+    Makefile = {
+      icon = "",
+      color = "#6d8086",
+      cterm_color = "66",
+      name = "RMakefile",
+    },
+  },
+  default = true
+}
