90dc024b51f3cbdce590833e277ceafba080f625
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2022-02-11 00:07:39 -0300

    update telescope configuration.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,12 +4,42 @@ require('packer').loader('telescope-fzf-native.nvim')
 
 telescope.setup {
   defaults = {
-    prompt_prefix = '🔭 ',
-    layout_strategy = 'bottom_pane',
-    sorting_strategy = 'ascending',
-    layout_config = {
-      height = 20,
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
     },
+    prompt_prefix = "   ",
+    selection_caret = "  ",
+    layout_strategy = "horizontal",
+    sorting_strategy = 'ascending',
+    initial_mode = "insert",
+    entry_prefix = "  ",
+    selection_strategy = "reset",
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.55,
+        results_width = 0.8,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
+    path_display = { "truncate" },
+    winblend = 0,
+    border = {},
+    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    color_devicons = true,
+    use_less = true,
+    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
   },
 
   pickers = {
