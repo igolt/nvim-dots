59dfc566694bbea62b522ecde75aeb5afdcf93bd
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2025-04-07 21:39:10 -0300

    fix: terminal mapping to go to the window in the right

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -76,7 +76,7 @@ vim.keymap.set(
 vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h')
 vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w>j')
 vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w>k')
-vim.keymap.set('t', '<s-C-l>', '<C-\\><C-n><C-w>l')
+vim.keymap.set('t', '<C-L>', '<C-\\><C-n><C-w>l')
 
 -- Telescope mappings
 vim.keymap.set('n', '<C-p>', '<Cmd>Telescope find_files<CR>')
