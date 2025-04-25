505d90cb0f9ef109a5330c1d1cb2ed98a7401f65
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2025-04-24 23:23:51 -0300

    chore: removing autopairs rules that doesn't work

--- lua/plugins/config/autopairs.lua
+++ lua/plugins/config/autopairs.lua
@@ -1,17 +1,6 @@
 local autopairs = require('nvim-autopairs')
 local Rule = require('nvim-autopairs.rule')
 
----@param opening string
----@param closing string
-local function with_space_between(opening, closing)
-  local cws = closing:match('^%s*(.-)%s*$')
-  local match = '.%' .. cws
-  return Rule(opening, closing)
-    :with_pair(function() return false end)
-    :with_del(function() return false end)
-    :with_move(function(opts) return opts.prev_char:match(match) ~= nil end)
-    :use_key(cws)
-end
 
 autopairs.setup()
 autopairs.add_rules {
@@ -19,7 +8,4 @@ autopairs.add_rules {
     local pair = opts.line:sub(opts.col - 1, opts.col)
     return vim.tbl_contains({ '()', '[]', '{}' }, pair)
   end),
-  with_space_between('( ', ' )'),
-  with_space_between('{ ', ' }'),
-  with_space_between('[ ', ' ]'),
 }
