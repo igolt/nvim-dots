e4334ffc52dc57643f1a45d76ad785df003b208a
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2025-04-24 23:28:27 -0300

    chore: update autopairs `Rule(' ', ' ')` to not be used in Markdown files

--- lua/plugins/config/autopairs.lua
+++ lua/plugins/config/autopairs.lua
@@ -1,11 +1,15 @@
 local autopairs = require('nvim-autopairs')
 local Rule = require('nvim-autopairs.rule')
 
+local function cond(opts)
+  if vim.bo.filetype == 'markdown' then
+    return false
+  end
+  local pair = opts.line:sub(opts.col - 1, opts.col)
+  return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+end
 
 autopairs.setup()
 autopairs.add_rules {
-  Rule(' ', ' '):with_pair(function(opts)
-    local pair = opts.line:sub(opts.col - 1, opts.col)
-    return vim.tbl_contains({ '()', '[]', '{}' }, pair)
-  end),
+  Rule(' ', ' '):with_pair(cond):with_del(cond),
 }
