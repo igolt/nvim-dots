0374d889ef40ea75cff916a9d426b6c13cf7170d
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2025-07-28 09:00:03 -0300

    feat: enable docker compose language server

--- lua/plugins/config/lspconfig/servers.lua
+++ lua/plugins/config/lspconfig/servers.lua
@@ -6,6 +6,7 @@ local util = require('plugins.config.lspconfig.util')
 
 ---@type table<string, lspconfig.Config>
 local servers = {
+  docker_compose_language_service = {},
   dockerls = {},
   prismals = {},
   bashls = {},
