9ee16334bc0d4e6faba6958f8f34219311bbb37a
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2025-10-30 18:09:56 -0300

    chore: trigger completion manually

--- lua/plugins/config/cmp.lua
+++ lua/plugins/config/cmp.lua
@@ -1,20 +1,31 @@
 local cmp = require('cmp')
 
 cmp.setup {
+  completion = {
+    autocomplete = false,
+  },
+
   snippet = {
     expand = function(args) require('luasnip').lsp_expand(args.body) end,
   },
 
   mapping = cmp.mapping.preset.insert {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-n>'] = cmp.mapping(function(_)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        cmp.complete()
+      end
+    end),
+    -- ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = false,
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
     },
 
     ['<Tab>'] = cmp.mapping(function(fallback)
