72f0e8db63745bca84ddbb60cbd43cd2cb358206
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2025-11-06 17:46:03 -0300

    feat: C code runner

--- lua/core/commands/code-runner.lua
+++ lua/core/commands/code-runner.lua
@@ -1,3 +1,4 @@
+local fs = require('lib.fs')
 local Terminal = require('toggleterm.terminal').Terminal
 
 ---@param cmd string
@@ -16,10 +17,30 @@ local code_runner = function(cmd)
   end
 end
 
+local function c_code_runner()
+  local filepath = vim.api.nvim_buf_get_name(0)
+  local basename = fs.basename(filepath)
+  local out = ('/tmp/%s.a.out'):format(basename)
+
+  local cc_and_run = ('cc %q -o %q; %q'):format(filepath, out, out)
+  local rm_out = ('rm -f %q'):format(out)
+
+  local term = Terminal:new {
+    cmd = cc_and_run
+      .. '; printf "\\nPress enter..."; read -r;'
+      .. rm_out
+      .. '; exit',
+    direction = 'horizontal',
+    close_on_exit = true,
+  }
+  term:toggle()
+end
+
 local code_runners = {
   python = code_runner('python'),
   lua = code_runner('lua'),
   javascript = code_runner('node'),
+  c = c_code_runner,
 }
 
 return {
